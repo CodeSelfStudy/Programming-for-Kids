ae36cb44d630e28ad40a37a32b3278e8851ad639
J. Cohen <hostelmarketing@gmail.com>
2016-01-01 18:48:59 -0800

    Add function_machines.py script

---
+++ python/function_machines.py
@@ -0,0 +1,22 @@
+"""From function machines to functions."""
+
+
+def add_one(num):
+    """Adds 1 to a number."""
+    return num + 1
+
+
+def add_two(num):
+    """Adds 2 to a number."""
+    return num + 2
+
+
+def square(num):
+    """Squares a number."""
+    return num * num
+
+
+def cube(num):
+    """Cubes a number."""
+    return num * num * num
+
