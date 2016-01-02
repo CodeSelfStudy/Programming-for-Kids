9f0f5ce2e0478eabf4489947547f6fc79ba71195
J. Cohen <hostelmarketing@gmail.com>
2016-01-01 19:23:44 -0800

    Fix for PEP8

--- python/cat_creator.py
+++ python/cat_creator.py
@@ -1,7 +1,11 @@
 """Creates a cat."""
 
+
 class Cat():
+    """A cat."""
+
     def __init__(self, name, mood):
+        """Initialize cat."""
         self.name = name
         self.mood = mood
 
@@ -22,29 +26,32 @@ class Cat():
         print('{} is climing the tree.'.format(self.name))
 
     def info(self):
-        """Shows the attributes for the cat."""
+        """Show the attributes for the cat."""
         print(self.__dict__)
 
     def change_mood(self, new_mood):
-        """Changes the mood of the cat."""
+        """Change the mood of the cat."""
         old_mood = self.mood
         self.mood = new_mood
         print("{}'s mood was changed from {} to {}!".format(
-                                                        self.name,
-                                                        old_mood,
-                                                        self.mood))
+            self.name,
+            old_mood,
+            self.mood))
         self.meow()
 
 
 def create_cat():
-    """Creates a cat."""
+    """Create a cat."""
     name = input("What is the cat's name? ")
     mood = input("What is the cat's mood? ")
     print("Your {} cat, {}, has been created.".format(mood, name))
     return Cat(name, mood)
 
+
 def main():
-    print("Type something like 'cookie = create_cat()' to begin. (Don't use quotes.)")
+    """Run the program."""
+    print("""Type something like 'cookie = create_cat()' to begin.
+            (Don't use quotes.)""")
 
 if __name__ == '__main__':
     main()
